07707fd319277d75dcfa93ebd8f5b47389d7b281
Rati_Aptsiauri <aptsiaurirati@gmail.com>
2026-06-10 02:18:38 +0400

    samas

--- cubeide/4wheel/Debug/Core/Src/main.d
+++ cubeide/4wheel/Debug/Core/Src/main.d
@@ -22,16 +22,17 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
- ../Core/Inc/adc.h ../Core/Inc/main.h ../Core/Inc/dma.h ../Core/Inc/tim.h \
- ../Core/Inc/gpio.h ../Core/Inc/MySrc/close_ir.h \
+ ../Core/Inc/adc.h ../Core/Inc/main.h ../Core/Inc/dma.h ../Core/Inc/i2c.h \
+ ../Core/Inc/tim.h ../Core/Inc/gpio.h ../Core/Inc/MySrc/close_ir.h \
  ../Core/Inc/MySrc/debug_vars.h ../Core/Inc/MySrc/light.h \
  ../Core/Inc/MySrc/median_calculator.h ../Core/Inc/MySrc/move.h \
  ../Core/Inc/MySrc/wheel.h ../Core/Inc/MySrc/platform_adapter.h \
  ../Core/Inc/MySrc/seek.h ../Core/Inc/MySrc/sharp_manager.h \
- ../Core/Inc/MySrc/sharp_suggest.h ../Core/Inc/MySrc/debug_light.h
+ ../Core/Inc/MySrc/sharp_suggest.h ../Core/Inc/MySrc/ws2812b.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
 ../Core/Inc/stm32f1xx_hal_conf.h:
@@ -56,12 +57,14 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
 ../Core/Inc/adc.h:
 ../Core/Inc/main.h:
 ../Core/Inc/dma.h:
+../Core/Inc/i2c.h:
 ../Core/Inc/tim.h:
 ../Core/Inc/gpio.h:
 ../Core/Inc/MySrc/close_ir.h:
@@ -74,4 +77,4 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Core/Inc/MySrc/seek.h:
 ../Core/Inc/MySrc/sharp_manager.h:
 ../Core/Inc/MySrc/sharp_suggest.h:
-../Core/Inc/MySrc/debug_light.h:
+../Core/Inc/MySrc/ws2812b.h:
