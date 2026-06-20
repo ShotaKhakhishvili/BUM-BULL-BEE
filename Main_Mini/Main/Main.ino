2f57ac187c93c1f1c0f218d49c7070d64d45e800
Shota Khakhishvili <94229156+ShotaKhakhishvili@users.noreply.github.com>
2026-06-20 12:01:30 +0200

    no line detection

--- Main_Mini/Main/Main.ino
+++ Main_Mini/Main/Main.ino
@@ -1,14 +1,12 @@
 #include "Defines.hpp"
 #include "SharpMedian.hpp"
 #include "Range.hpp"
-#include "Infrared.hpp"
 
 #include "Move.hpp"
 #include "Behavior.hpp"
 #include "RunState.hpp"
 
 Range range;
-Infrared ir1, ir2;
 unsigned long lastDebugPrint = 0;
 
 void setup()
@@ -20,24 +18,21 @@ void setup()
     Move::Init();           // TB6612FNG motor pins
 
     range.Init();           // Sharp (A3) + ToF (I2C: A4/A5)
-    ir1.Init(IR_READS_ANALOG ? IR1_ANALOG : IR1, IR_READS_ANALOG);
-    ir2.Init(IR_READS_ANALOG ? IR2_ANALOG : IR2, IR_READS_ANALOG);
 
     RunState::Init();       // start (D2) / stop (D1) interrupts
 }
 
 void loop()
 {
-    if(RunState::IsRunning())
-        Move::Walk(FORWARD, 200);
-    else
+    if(!RunState::IsRunning())
+    {
         Move::Walk(FORWARD, 0);
+        return;
+    }
 
-    // --- normal behavior disabled for start test ---
-    /*
     double front = range.Distance();
 
-    Behavior::Update(front);
+    Behavior::Update(front);    // SEEK (spin) / CHASE (catch)
 
     if(millis() - lastDebugPrint >= 1000)
     {
@@ -47,10 +42,7 @@ void loop()
         Behavior::DebugPrint();
         Move::DebugPrint();
         range.DebugPrint();
-        ir1.DebugPrint();
-        ir2.DebugPrint();
 
         lastDebugPrint = millis();
     }
-    */
 }
